5dd06030b6b13bcbd960dd001c38ec414594c4cd
masonkarsevar <masonkarsevar@gmail.com>
2019-08-07 08:26:24 -0700

    basic form structure finished. WIP: data handling using formik

--- user-onboarding-app/src/App.js
+++ user-onboarding-app/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import SignupForm from './components/SignupForm';
 
 function App() {
   return (
     <div className="App">
-      
+      <SignupForm />
     </div>
   );
 }
--- user-onboarding-app/src/components/SignupForm.js
+++ user-onboarding-app/src/components/SignupForm.js
@@ -3,6 +3,21 @@ import {withFormik, Form, Field} from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
 
-function SignupForm() {
-    
+function SignupForm({values}) {
+    return (
+        <div className='loginForm'>
+            <Form>
+                <Field type='text' name='username' placeholder='Name' />
+                <Field type='email' name='email' placeholder='Email' />
+                <Field type='password' name='password' placeholder='Password' />
+                {/* <label>
+                    <Field type='checkbox' name='tos' checked={values.tos} />
+                    Accept Terms of Service
+                </label> */}
+                <button>Submit!</button>
+            </Form>
+        </div>
+    )
 }
+
+export default SignupForm;
